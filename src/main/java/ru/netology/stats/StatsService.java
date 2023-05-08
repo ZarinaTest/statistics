c473cdec5a3e594751cc3b2720802afd8d84d170
Zarina <Zarina-zayka@yandex.ru>
2023-05-08 22:10:48 +0500

    Gitignore added

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,68 @@
+package ru.netology.stats;
+
+public class StatsService {
+
+    public int calculateSum(int[] sales) {
+        int sum = 0;
+        for (int sale : sales) {
+            sum = sum + sale;
+        }
+        return sum;
+    }
+
+    public int calculateAvg(int[] sales) {
+        return calculateSum(sales) / sales.length;
+    }
+
+    public int sumMaxMonth(long[] sales) {
+        int maxMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (long sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return maxMonth + 1;
+    }
+
+
+    public int sumMinMonth(long[] sales) {
+        int minMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (long sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return minMonth + 1;
+    }
+
+    public int amountMonthUnderAvg(int[] sales) {
+        int count = 0;
+        int salesAvg = calculateAvg(sales); // Системе легче производить рассчет
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] < salesAvg) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int amountMonthOverAvg(int[] sales) {
+        int count = 0;
+        int salesAvg = calculateAvg(sales);
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] > salesAvg) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
